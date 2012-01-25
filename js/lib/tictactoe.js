18bd8378d81bd61f45619796413f7bccc9466169
Jonathan Moore <jon@jonfm.co.uk>
2012-01-25 12:12:14 +0100

    added stalemate test case (fails) and converted tictactoe to a pseudo-object

--- js/lib/tictactoe.js
+++ js/lib/tictactoe.js
@@ -1,79 +1,84 @@
 define(
     ["lib/jquery", "lib/mustache"],
     function ($, mustache) {
-
         // The board template is stored in the DOM at #board_template
         var board_template = $("#board_template").html();
 
-        /* TODO: convert the board (and other vars at this scope) to instance
-           attribute so one game can be an "object" instance. Is there a more
-           idiomatic way to do this in JS?
-        */
-        var board = [];
-        for (x in [0, 1, 2]) {
-            for (y in [0, 1, 2]) {
-                if (typeof board[x] === "undefined") board[x] = [];
-                board[x][y] = { val: 0 };
-            }
-        }
-        for (x in [0, 1, 2]) {
-            for (y in [0, 1, 2]) {
-                board[x][y].links = [
-                    board[(x == 0 ? 3 : x) - 1][y], // west
-                    board[(x == 0 ? 3 : x) - 1][(y == 0 ? 3 : y) - 1], // south west
-                    board[x][(y == 0 ? 3 : y) - 1] // south
-                ];
-            }
-        }
+        // construct for the pseudo-object
+        function init () {
 
-        // render a template stored in board
-        function render_board () {
-            $('#board').html( mustache.render(board_template, { board: board } ) );
-        }
-
-        // TODO: try to make these object attributes
-        var last_move;
-        var win = false;
-        function move ( xo, pos ) {
-
-            // Is this your move?
-            if ( last_move == xo ) return false;
-
-            // Is there already a token in this position?
-            if (
-                board[ pos[0] ][ pos[1] ].val == "x"
-             || board[ pos[0] ][ pos[1] ].val == "o"
-            ) return false;
-
-            // This move is allowed so we place the token:
-            board[ pos[0] ][ pos[1] ].val = xo;
-            // Record this token as the last to move
-            last_move = xo;
-
-            /* To check a winner we want to look for a line from this move in
-               any of the three directions. Conveniently our board has the wrap-
-               around links property to make this check convenient.
-            */
-            for (dir in [0, 1, 2]) {
-                if (
-                    board[ pos[0] ][ pos[1] ].links[dir].val == xo
-                    && board[ pos[0] ][ pos[1] ].links[dir].links[dir].val == xo
-                ) {
-                    win = xo;
+            var board = [];
+            var last_move;
+            var win;
+            function init () {
+                last_move = false;
+                win = false;
+                for (x in [0, 1, 2]) {
+                    for (y in [0, 1, 2]) {
+                        if (typeof board[x] === "undefined") board[x] = [];
+                        board[x][y] = { val: 0 };
+                    }
+                }
+                for (x in [0, 1, 2]) {
+                    for (y in [0, 1, 2]) {
+                        board[x][y].links = [
+                            board[(x == 0 ? 3 : x) - 1][y], // west
+                            board[(x == 0 ? 3 : x) - 1][(y == 0 ? 3 : y) - 1], // south west
+                            board[x][(y == 0 ? 3 : y) - 1] // south
+                        ];
+                    }
                 }
             }
+            init();
 
-            return true;
+            // render a template stored in board
+            function render_board ( selector ) {
+                $(selector).html( mustache.render(board_template, { board: board } ) );
+            }
+
+            function move ( xo, pos ) {
+
+                // Is this your move?
+                if ( last_move == xo ) return false;
+
+                // Is there already a token in this position?
+                if (
+                    board[ pos[0] ][ pos[1] ].val == "x"
+                 || board[ pos[0] ][ pos[1] ].val == "o"
+                ) return false;
+
+                // This move is allowed so we place the token:
+                board[ pos[0] ][ pos[1] ].val = xo;
+                // Record this token as the last to move
+                last_move = xo;
+
+                /* To check a winner we want to look for a line from this move in
+                   any of the three directions. Conveniently our board has the wrap-
+                   around links property to make this check convenient.
+                */
+                for (dir in [0, 1, 2]) {
+                    if (
+                        board[ pos[0] ][ pos[1] ].links[dir].val == xo
+                        && board[ pos[0] ][ pos[1] ].links[dir].links[dir].val == xo
+                    ) {
+                        win = xo;
+                    }
+                }
+
+                return true;
+            }
+
+            function winner () {
+                return win;
+            }
+
+            return {
+                render_board: render_board,
+                move:         move,
+                winner:       winner,
+                init:         init
+            };
         }
-
-        function winner () {
-            return win;
-        }
-
-        return {
-            render_board: render_board,
-            move:         move,
-            winner:       winner
-        };
+        return init;
     }
 );
--- js/test.js
+++ js/test.js
@@ -1,8 +1,11 @@
 define(
     ["lib/jquery", "lib/qunit", "lib/tictactoe"],
-    function ($, qunit, game) {
+    function ($, qunit, tictactoe) {
+
         module("tictactoe");
+
         test("a game with a winner", function () {
+            var game = tictactoe();
             ok( game.move("o", [1,1]), "placed the first o in the centre" );
             ok( !game.move("o", [2,1]), "o cannot move again" );
             ok( game.move("x", [0,1]), "placed the first x at 0,0" );
@@ -17,7 +20,27 @@ define(
             ok( game.move("o", [1,0]), "placed o" );
             equals( game.winner(), "o", "o wins the game" );
 
-            game.render_board();
+            game.render_board('#winning_board');
+
+        });
+
+        test("a game with a stalemate", function () {
+            var game = tictactoe();
+            ok( game.move("o", [1,1]), "placed the first o in the centre" );
+            ok( !game.move("o", [2,1]), "o cannot move again" );
+            ok( game.move("x", [0,1]), "placed the first x at 0,0" );
+            ok( !game.move("o", [0,1]), "cannot place another piece at 0,0" );
+            ok( game.move("o", [0,0]), "placed o" );
+            ok( game.move("x", [2,2]), "placed x" );
+            ok( !game.winner(), "no winner yet" );
+            ok( game.move("o", [2,0]), "placed o" );
+            ok( !game.winner(), "no winner yet" );
+            ok( game.move("x", [0,2]), "placed x" );
+            ok( !game.winner(), "no winner yet" );
+            ok( game.move("o", [1,0]), "placed o" );
+            ok( !game.winner(), "no winner" );
+
+            game.render_board('#stalemate_board');
 
         });
     }
